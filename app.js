7ab04235b3ad28bd73677dc57ef4e58af3d3f309
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-02-12 10:07:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)","#f15025","aqua","magenta","thistle","#76ff7a","yellow","pink","gray","orange","brown"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+     const randomNumber = getRandomNumber();
+      console.log(randomNumber)
+
+     document.body.style.backgroundColor = colors[randomNumber];
+     color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
